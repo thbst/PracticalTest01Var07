699ce8b0530fac4ece1f61a7aa0ee5bb0e952bf2
Cristian Lupu <cristianlupu@gmail.com>
2016-04-01 13:37:51 +0300

    [code] added second activity GUI

--- PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
+++ PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
@@ -1,52 +1,65 @@
 package ro.pub.cs.systems.eim.practicaltest01var07;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+
 
 public class PracticalTest01Var07MainActivity extends AppCompatActivity {
+    private Button navButton = null;
+    private final String TAG = "EIM";
+    private final String MY_ACTION = "ro.pub.cs.systems.eim.practicaltest01var07.intent.action.PracticalTest01Activity";
+
+    private class HandleClick implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            navButton = (Button) findViewById(R.id.nav_button);
+
+            switch (view.getId()) {
+                case R.id.nav_button:
+                    Log.d(TAG, "nav_button");
+//                    final String THE_ACTION="ro.pub.cs.systems.eim.lab04.intent.action.PracticalTest01Activity";
+//                    final String THE_CATEGORY="android.intent.category.DEFAULT";
+                    Intent intent = new Intent();
+                    intent.setAction(MY_ACTION);
+//                    intent.addCategory(THE_CATEGORY);
+//                    intent.putExtra(MY_ACTION+".count1", count1);
+                    try {
+//                        startActivityForResult(intent,ID);
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                        Log.d("TAG", "Secondary Activity not found");
+                    }
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var07_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        navButton = (Button) findViewById(R.id.nav_button);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        navButton.setOnClickListener(new HandleClick());
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_practical_test01_var07_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07SecondaryActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest01var07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest01Var07SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var07_secondary);
+    }
+}
